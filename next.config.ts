c1a86114c587cd6a5ba7fa6e1746813dc24d827c
alli1378 <53428896+alli1378@users.noreply.github.com>
2026-08-17 11:05:28 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/babakkasiri",
-  assetPrefix:"/babakkasiri",
     images: {
     unoptimized: true,
   },
