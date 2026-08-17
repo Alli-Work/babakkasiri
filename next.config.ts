4ebfc933d7535c77a0b346a607229629bb17f67e
alli.nasr <alli.nasr1378@gmail.com>
2026-08-17 12:08:34 +0430

    change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/bakkasiri",
+  output: "export",
+  basePath: "/babakkasiri",
     images: {
     unoptimized: true,
   },
